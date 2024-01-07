247015b383c4444d7b2497512f5fa6f1598a1cee
ChunHou20c <youmustbekiddingmeshit@gmail.com>
2024-01-07 21:03:15 +0800

    update main script for generating descriptors

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from descriptor_generator.descriptor_generator import Molecule_Aggregate
 
-molecules = Molecule_Aggregate.from_path("/home/chunhou/Dev/python/padelpy_descriptor_generation/data/molecules/optimized/fixed_first_batch/", 5)
+molecules = Molecule_Aggregate.from_path("/home/chunhou/Dev/python/padelpy_descriptor_generation/data/molecules/optimized/fixed_first_batch/", 10)
 molecules.generate_padelpy_fingerprint(molecules.molecules.keys(), max_run_time = 100, regenerate = False)
+molecules.generate_padelpy_2D_descriptor(molecules.molecules.keys(), max_run_time = 100, regenerate = False)
+molecules.generate_padelpy_3D_descriptor(molecules.molecules.keys(), max_run_time = 100, regenerate = False)
 print(molecules)
-status = molecules.check_padelpy_descriptor_empty_list()
-print(status)
+fingerprint_status = molecules.check_padelpy_fingerprint_empty_list()
+print(fingerprint_status)
 
-for (key, incomplete_list) in status.items():
-    molecules.generate_padelpy_fingerprint(incomplete_list, max_run_time = 1000, regenerate=True)
+descriptor_2D_status = molecules.check_padelpy_descriptor_2D_empty_list()
+print(descriptor_2D_status)
 
-status = molecules.check_padelpy_descriptor_empty_list()
-print(status)
+descriptor_3D_status = molecules.check_padelpy_descriptor_3D_empty_list()
+print(descriptor_3D_status)
+# for (key, incomplete_list) in fingerprint_status.items():
+#     molecules.generate_padelpy_fingerprint(incomplete_list, max_run_time = 1000, regenerate=True)
+# 
+# status = molecules.check_padelpy_fingerprint_empty_list()
+# print(status)
 #descriptors = molecules.generate_rdkit_descriptor()
 #print(descriptors)
