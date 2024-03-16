dcbafcf1e827831837c3fe636ceea662128f656d
Chun Hou Tan <chunhou@cpu03.dicc.um.edu.my>
2024-03-16 13:06:57 +0800

    fix the check content script written earlier

--- check_content.py
+++ check_content.py
@@ -18,36 +18,35 @@ def cache_molecule(molecule:Molecule_Aggregate, filename: str)->None:
 
 def load_molecule(filename:str)->Molecule_Aggregate:
 
-    with open(filename, 'wb') as f:
+    with open(filename, 'rb') as f:
 
         molecule = pickle.load(f)
 
         return molecule
 
-with open("config.json") as f:
+with open("config.json", "r") as f:
     configs = json.load(f)
 
 
-    threads = configs.get("max_threads")
-    runtime_multiplier = configs.get("runtime_multiplier")
-    molecule_cache = configs.get("molecule_cache")
+    molecule_cache = "/home/user/chunhou/Data/cache.pickle"
+    print(molecule_cache)
 
     if path.isfile(molecule_cache):
         molecules = load_molecule(molecule_cache)
 
-        for key, value in molecules.fingerprint_dict.values():
+        for key, value in molecules.fingerprint_dict.items():
 
             print(key)
             print(value)
 
 
-        for key, value in molecules.descriptor_2D_dict.values():
+        for key, value in molecules.descriptor_2D_dict.items():
 
             print(key)
             print(value)
 
 
-        for key, value in molecules.descriptor_3D_dict.values():
+        for key, value in molecules.descriptor_3D_dict.items():
 
             print(key)
             print(value)
